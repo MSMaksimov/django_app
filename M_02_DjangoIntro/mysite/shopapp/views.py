172706ceeea88b159c2c5f417cd0e15db860b8d7
Максим Максимов <mmax77@mail.ru>
2023-03-21 14:43:44 +0500

    app is ready

--- M_02_DjangoIntro/mysite/shopapp/views.py
+++ M_02_DjangoIntro/mysite/shopapp/views.py
@@ -5,7 +5,14 @@ from django.shortcuts import render
 
 
 def shop_index(request: HttpRequest):
+    products = [
+        ('Lapptop', 1999),
+        ('Desktop', 2999),
+        ('Smartphone', 999),
+
+    ]
     context = {
         "time_running": default_timer(),
+        "products": products,
     }
     return render(request, 'shopapp/shop-index.html', context=context)
