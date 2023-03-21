c4afc8f59ef15d4a24484a715a46e06d1a201bde
Максим Максимов <mmax77@mail.ru>
2023-03-21 14:07:23 +0500

    templates folder created

--- M_02_DjangoIntro/mysite/shopapp/views.py
+++ M_02_DjangoIntro/mysite/shopapp/views.py
@@ -1,3 +1,11 @@
+from timeit import default_timer
+
+from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
 
-# Create your views here.
+
+def shop_index(request: HttpRequest):
+    context = {
+        "time_running": default_timer(),
+    }
+    return render(request, 'shopapp/shop-index.html', context=context)
