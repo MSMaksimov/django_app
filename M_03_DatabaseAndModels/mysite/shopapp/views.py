e8d4a2ad619fedf1e1b9c6f1e6ad6cdbce39b3c6
Максим Максимов <mmax77@mail.ru>
2023-03-24 15:20:48 +0500

    Make pause commit

--- M_03_DatabaseAndModels/mysite/shopapp/views.py
+++ M_03_DatabaseAndModels/mysite/shopapp/views.py
@@ -1,9 +1,11 @@
+from timeit import default_timer
+
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
 
 
 def shop_index(request: HttpRequest):
-    print(request.path)
-    print(request.method)
-    print(request.headers)
-    return HttpResponse("<h1>Hello world!</h1>")
+    context = {
+        "time_running": default_timer(),
+    }
+    return render(request, 'shopapp/shop-index.html', context=context)
