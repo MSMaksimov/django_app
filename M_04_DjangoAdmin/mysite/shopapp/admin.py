5276dd8fd5dd3bdfe077776f0f1574025b8f05e7
Максим Максимов <mmax77@mail.ru>
2023-03-31 17:48:06 +0500

    pause

--- M_04_DjangoAdmin/mysite/shopapp/admin.py
+++ M_04_DjangoAdmin/mysite/shopapp/admin.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 
-from .models import Product
+from .models import Product, Order
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = "pk", "name", "description_short", "price", "discount"
     list_display_links = "pk", "name"
+    ordering = "name", "pk"
+    search_fields = "name", "description", "price", "discount"
 
     def description_short(self, obj: Product) -> str:
         if len(obj.description) < 48:
@@ -14,3 +16,14 @@ class ProductAdmin(admin.ModelAdmin):
         return obj.description[:48] + "..."
 
 # admin.site.register(Product, ProductAdmin)
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = "delivery_address", "promocode", "created_at", "user"
+
+    def get_queryset(self, request):
+        return Order.objects.select_related("user")
+    #
+    # def user_verbose(self, obj: Order) -> str:
+    #     return obj.user.f
