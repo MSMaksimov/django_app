607b7bc3081e5764e09ccda5364a55a610bfad7e
Максим Максимов <mmax77@mail.ru>
2023-03-31 20:26:31 +0500

    pause v2.0

--- M_04_DjangoAdmin/mysite/shopapp/admin.py
+++ M_04_DjangoAdmin/mysite/shopapp/admin.py
@@ -3,12 +3,33 @@ from django.contrib import admin
 from .models import Product, Order
 
 
+class OrderInline(admin.TabularInline):
+    model = Product.orders.through
+
+
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
+    inlines = [
+        OrderInline,
+    ]
     list_display = "pk", "name", "description_short", "price", "discount"
     list_display_links = "pk", "name"
     ordering = "name", "pk"
     search_fields = "name", "description", "price", "discount"
+    fieldsets = [
+        (None, {
+            "fields": ("name", "description"),
+        }),
+        ("Price options", {
+            "fields": ("price", "discount"),
+            "classes": ("wide", "collapse",),
+        }),
+        ("Extra options", {
+            "fields": ("archived",),
+            "classes": ("collapse",),
+            "description": "Extra options. Fild 'archived' is for soft delete",
+        })
+    ]
 
     def description_short(self, obj: Product) -> str:
         if len(obj.description) < 48:
@@ -18,12 +39,21 @@ class ProductAdmin(admin.ModelAdmin):
 # admin.site.register(Product, ProductAdmin)
 
 
+# class ProductInline(admin.TabularInline):
+class ProductInline(admin.StackedInline):
+
+    model = Order.products.through
+
+
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = "delivery_address", "promocode", "created_at", "user"
+    inlines = [
+        ProductInline,
+    ]
+    list_display = "delivery_address", "promocode", "created_at", "user_verbose"
 
     def get_queryset(self, request):
-        return Order.objects.select_related("user")
-    #
-    # def user_verbose(self, obj: Order) -> str:
-    #     return obj.user.f
+        return Order.objects.select_related("user").prefetch_related("products")
+
+    def user_verbose(self, obj: Order) -> str:
+        return obj.user.first_name or obj.user.username
