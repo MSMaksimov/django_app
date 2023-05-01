55a71eac520f868311ed15417860f0570531fa64
Максим Максимов <mmax77@mail.ru>
2023-05-01 19:40:26 +0500

    Added class ProductExportViewTestCase(TestCase)

--- 10_Testing/mysite/shopapp/tests.py
+++ 10_Testing/mysite/shopapp/tests.py
@@ -1,6 +1,7 @@
 from random import choices
 from string import ascii_letters
 
+from django.conf import settings
 from django.contrib.auth.models import User, Permission
 from django.test import TestCase
 from django.urls import reverse
@@ -83,3 +84,55 @@ class ProductsListViewTestCase(TestCase):
             transform=lambda p: p.pk,
         )
         self.assertTemplateUsed(response, 'shopapp/products-list.html')
+
+
+class OrdersListViewTestCase(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.user = User.objects.create_user(username="test_user", password="qwerty")
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.user.delete()
+
+    def setUp(self) -> None:
+        self.client.force_login(self.user)
+
+    def test_orders_view(self):
+        response = self.client.get(reverse("shopapp:orders_list"))
+        self.assertContains(response, "Orders")
+
+    def test_orders_view_not_authenticated(self):
+        self.client.logout()
+        response = self.client.get(reverse("shopapp:orders_list"))
+        self.assertEqual(response.status_code, 302)
+        self.assertIn(str(settings.LOGIN_URL), response.url)
+
+
+class ProductExportViewTestCase(TestCase):
+    fixtures = [
+        "product-fixture.json",
+    ]
+
+    def test_get_products_view(self):
+        response = self.client.get(
+            reverse("shopapp:products-export")
+        )
+        self.assertEqual(response.status_code, 200)
+        products = Product.objects.order_by("pk").all()
+        expected_data = [
+            {
+                "pk": product.pk,
+                "name": product.name,
+                "price": str(product.price),
+                "description": product.description,
+                "archived": product.archived,
+            }
+            for product in products
+        ]
+        products_data = response.json()
+        self.assertEqual(
+            products_data["products"],
+            expected_data,
+        )
--- 10_Testing/mysite/shopapp/views.py
+++ 10_Testing/mysite/shopapp/views.py
@@ -1,6 +1,6 @@
 from timeit import default_timer
 from django.contrib.auth.models import Group
-from django.http import HttpResponse, HttpRequest, HttpResponseRedirect
+from django.http import HttpResponse, HttpRequest, HttpResponseRedirect, JsonResponse
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.urls import reverse_lazy
 from django.views import View
@@ -149,3 +149,19 @@ class OrderCreateView(CreateView):
     fields = "delivery_address", "promocode", "user", "products"
     template_name_suffix = "_create_form"
     success_url = reverse_lazy("shopapp:orders_list")
+
+
+class ProductsDataExportView(View):
+    def get(self, request: HttpRequest) -> JsonResponse:
+        products = Product.objects.order_by("pk").all()
+        products_data = [
+            {
+                "pk": product.pk,
+                "name": product.name,
+                "price": product.price,
+                "description": product.description,
+                "archived": product.archived,
+            }
+            for product in products
+        ]
+        return JsonResponse({"products": products_data})
