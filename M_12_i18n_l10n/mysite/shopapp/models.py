8afa7eb3493eb46396189184b96e9784715fd58c
Максим Максимов <mmax77@mail.ru>
2023-06-01 13:56:03 +0500

    Added verbose_name and verbose_name_plural for models of shopapp

--- M_12_i18n_l10n/mysite/shopapp/models.py
+++ M_12_i18n_l10n/mysite/shopapp/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+from django.utils.translation import gettext_lazy as _
+
 
 def product_preview_directory_path(instance: "Product", filename: str) -> str:
     return "products/product_{pk}/preview/{filename}".format(
@@ -12,7 +14,8 @@ def product_preview_directory_path(instance: "Product", filename: str) -> str:
 class Product(models.Model):
     class Meta:
         ordering = ["name", "price"]
-        # verbose_name = _("Product")
+        verbose_name = _("Product")
+        verbose_name_plural = _("Products")
 
     name = models.CharField(max_length=100)
     description = models.TextField(null=False, blank=True)
@@ -40,6 +43,11 @@ class ProductImage(models.Model):
 
 
 class Order(models.Model):
+    class Meta:
+        ordering = ["user", "delivery_address"]
+        verbose_name = _("Order")
+        verbose_name_plural = _("Orders")
+
     delivery_address = models.TextField(null=True, blank=True)
     promocode = models.CharField(max_length=20, null=False, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
