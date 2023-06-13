16db7c603813891ed7f457cb02951de0a21d6559
Максим Максимов <mmax77@mail.ru>
2023-06-13 17:00:19 +0200

    Pause

--- M_15_queries/mysite/blogapp/urls.py
+++ M_15_queries/mysite/blogapp/urls.py
@@ -1,1 +1,13 @@
 from django.urls import path
+
+from .views import ArticleListView, ArticleCreateView
+
+app_name = "blogapp"
+
+urlpatterns = [
+    path('articles/', ArticleListView.as_view(), name='articles'),
+    path('create/', ArticleCreateView.as_view(), name='create'),
+
+]
+
+
--- M_15_queries/mysite/blogapp/views.py
+++ M_15_queries/mysite/blogapp/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.views.generic import ListView, CreateView, DetailView
+from blogapp.models import Article, Author
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+
+class ArticleListView(ListView):
+    model = Article
+    template_name = 'blogapp/article_list.html'
+    context_object_name = 'articles'
+    queryset = Article.objects.select_related('author', 'category').prefetch_related('tags').defer('content')
+
+
+class ArticleCreateView(LoginRequiredMixin, CreateView):
+    model = Article
+    template_name = 'blogapp/article_create.html'
+    fields = ['title', 'content', 'author', 'category', 'tags']
+    success_url = '/articles/create/success/'
+
+    def form_valid(self, form):
+        author_name = form.cleaned_data['author']
+        author, created = Author.objects.get_or_create(name=author_name)
+        form.instance.author = author
+        return super().form_valid(form)
