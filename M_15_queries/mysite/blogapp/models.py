a7886c928b3938c76539f3f118d1794f125e8327
Максим Максимов <mmax77@mail.ru>
2023-06-13 13:18:12 +0200

    Added models Author, Category, Tag and Article to BlogApp

--- M_15_queries/mysite/blogapp/models.py
+++ M_15_queries/mysite/blogapp/models.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    bio = models.TextField(null=False, blank=True)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=40)
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=20, null=False, blank=True)
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=200, null=False, blank=False)
+    content = models.TextField(null=False, blank=True)
+    pub_dat = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag, related_name="tags")
+
