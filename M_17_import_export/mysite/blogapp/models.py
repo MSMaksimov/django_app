c12da25b6284d1a617b498c66d4f1632e5cd40c8
Максим Максимов <mmax77@mail.ru>
2023-06-29 18:41:26 +0200

    Added class Article to blogapp

--- M_17_import_export/mysite/blogapp/models.py
+++ M_17_import_export/mysite/blogapp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    body = models.TextField(null=True, blank=True)
+    published_at = models.DateTimeField(null=True, blank=True)
