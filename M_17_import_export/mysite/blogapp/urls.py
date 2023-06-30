df96ba60fefcc5860ff28fc36c812b92bd1af2e2
Максим Максимов <mmax77@mail.ru>
2023-06-30 14:35:04 +0200

    Added RSS support

--- M_17_import_export/mysite/blogapp/urls.py
+++ M_17_import_export/mysite/blogapp/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from .views import (
     ArticlesListVew,
     ArticlesDetailView,
+    LatestArticlesFeed
 )
 
 app_name = "blogapp"
@@ -10,4 +11,5 @@ app_name = "blogapp"
 urlpatterns = [
     path("articles/", ArticlesListVew.as_view(), name='articles'),
     path("articles/<int:pk>/", ArticlesDetailView.as_view(), name='article'),
+    path("articles/latest/feed/", LatestArticlesFeed(), name='articles-feed'),
 ]
--- M_17_import_export/mysite/blogapp/views.py
+++ M_17_import_export/mysite/blogapp/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.contrib.syndication.views import Feed
 from django.views.generic import ListView, DetailView
+from django.urls import reverse, reverse_lazy
 
 from .models import Article
 
@@ -14,3 +15,25 @@ class ArticlesListVew(ListView):
 
 class ArticlesDetailView(DetailView):
     model = Article
+
+
+class LatestArticlesFeed(Feed):
+    title = "Blog articles (latest)"
+    description = "Updates on changes and addition blog articles"
+    link = reverse_lazy("blogapp:articles")
+
+    def items(self):
+        return (
+            Article.objects
+            .filter(published_at__isnull=False)
+            .order_by("-published_at")[:5]
+        )
+
+    def item_title(self, item: Article):
+        return item.title
+
+    def item_description(self, item: Article):
+        return item.body[:200]
+
+    def item_link(self, item: Article):
+        return reverse("blogapp:article", kwargs={"pk": item.pk})
